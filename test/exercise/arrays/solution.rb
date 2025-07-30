fb222829449e8ec34dc5a66d2c513773194849aa
Vladislav Kostikov <vlad@kostikov.ru>
2025-07-30 19:15:18 +0300

    Implement #replace

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,15 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_value = array.first
+
+        array.each do |value|
+          max_value = value if value > max_value
+        end
+
+        array.map do |value|
+          value.positive? ? max_value : value
+        end
       end
 
       def search(_array, _query)
