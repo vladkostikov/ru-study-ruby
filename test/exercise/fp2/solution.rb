61bf64106c34ff163524d7437662651612328bb0
Vladislav Kostikov <vlad@kostikov.ru>
2025-07-31 10:04:31 +0300

    Implement MyArray#my_map

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -16,7 +16,11 @@ module Exercise
       end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        result = MyArray.new
+        my_each { |element| result << yield(element) }
+        result
+      end
 
       # Написать свою функцию my_compact
       def my_compact; end
