937c7cc00754e796b643d288b9165eef5684b336
Vladislav Kostikov <vlad@kostikov.ru>
2025-08-04 11:46:49 +0300

    Refactoring Arrays#replace

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,12 +2,7 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        max_value = array.first
-
-        array.each do |value|
-          max_value = value if value > max_value
-        end
-
+        max_value = find_max(array)
         array.map do |value|
           value.positive? ? max_value : value
         end
@@ -28,6 +23,14 @@ module Exercise
         start_i = middle_i + 1 if array[middle_i] < query
         end_i = middle_i - 1 if array[middle_i] > query
         b_search(array, query, start_i, end_i)
+      end
+
+      def find_max(array)
+        max_value = array.first
+        array.each do |value|
+          max_value = value if value > max_value
+        end
+        max_value
       end
     end
   end
